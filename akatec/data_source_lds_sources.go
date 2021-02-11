9c84302b1a67f56b107d97eb8636908f4634b4bb
Petr Artamonov <partamonov@schubergphilis.com>
2021-02-11 16:04:19 +0100

    working source data_source

--- akatec/data_source_lds_sources.go
+++ akatec/data_source_lds_sources.go
@@ -2,6 +2,10 @@ package akatec
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -19,8 +23,15 @@ func dataSourceLdsSources() *schema.Resource {
 					Schema: map[string]*schema.Schema{
 						"source_id":        {Type: schema.TypeString, Computed: true},
 						"source_type_name": {Type: schema.TypeString, Computed: true},
-						"source_cpcode":    {Type: schema.TypeString, Computed: true},
-						"source_products":  {Type: schema.TypeList, Computed: true},
+						"source_cpcode":    {Type: schema.TypeString, Computed: true, Optional: true},
+						"source_products": {
+							Type:     schema.TypeList,
+							Optional: true,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
 					},
 				},
 			},
@@ -38,22 +49,44 @@ func dataLdsSourcesRead(ctx context.Context, d *schema.ResourceData, m interface
 		return diag.FromErr(err)
 	}
 
-	for _, src := range *sources {
-		if err := d.Set("source_id", src.ID); err != nil {
-			return diag.FromErr(err)
-		}
-		d.SetId(src.ID)
-		if err := d.Set("source_type_name", src.Type); err != nil {
-			return diag.FromErr(err)
-		}
-		if err := d.Set("source_cpcode", src.CpCode); err != nil {
-			return diag.FromErr(err)
-		}
-		if err := d.Set("source_products", src.Products); err != nil {
-			return diag.FromErr(err)
-		}
+	srcList := make([]map[string]interface{}, 0, len(*sources))
 
+	for _, src := range *sources {
+		if src.Type == "edns" {
+			srcList = append(srcList, map[string]interface{}{
+				"source_id":        src.ID,
+				"source_type_name": src.Type,
+				"source_cpcode":    "N/A",
+				"source_products":  src.Products,
+			})
+		} else {
+			srcList = append(srcList, map[string]interface{}{
+				"source_id":        src.ID,
+				"source_type_name": src.Type,
+				"source_cpcode":    src.CpCode,
+				"source_products":  src.Products,
+			})
+		}
 	}
 
+	if err := d.Set("sources", srcList); err != nil {
+		return diag.FromErr(fmt.Errorf("%q", err.Error()))
+	}
+
+	jsonBody, err := json.Marshal(srcList)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(getSHAString(string(jsonBody)))
+
 	return diags
+}
+
+func getSHAString(rdata string) string {
+	h := sha1.New()
+	h.Write([]byte(rdata))
+
+	sha1hashtest := hex.EncodeToString(h.Sum(nil))
+	return sha1hashtest
 }
